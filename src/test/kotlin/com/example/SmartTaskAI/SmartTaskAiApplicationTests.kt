9adb6958dde10fe84f435464419f0433dac5ff4f
Vincent Deus Tedjarukmana <vtedjarukmana@student.ciputra.ac.id>
2026-04-16 20:38:02 +0700

    Initial commit & bootRun

---
+++ src/test/kotlin/com/example/SmartTaskAI/SmartTaskAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SmartTaskAI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartTaskAiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
