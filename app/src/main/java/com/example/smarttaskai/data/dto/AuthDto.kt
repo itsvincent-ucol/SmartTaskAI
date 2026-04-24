357efc9ab842d79e55d292b25456593f2fe8e03e
itsvincent-ucol <vtedjarukmana@student.ciputra.ac.id>
2026-04-24 16:18:14 +0700

    Delete app directory

--- app/src/main/java/com/example/smarttaskai/data/dto/AuthDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smarttaskai.data.dto
-
-data class LoginRequest(val email: String, val password: String)
-data class RegisterRequest(val name: String, val email: String, val password: String)
-data class AuthResponse(val token: String, val message: String)
